0248e3e087b7cc43933eaee5c043f9f6c89c8af8
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-05-07 21:18:42 -0300

    Update programaparimpar2.c

--- programaparimpar2.c
+++ programaparimpar2.c
@@ -5,13 +5,13 @@
 int main() {
     int numero;
 
-    printf("Digite um número inteiro: ");
+    printf("Digite um numero inteiro: ");
     scanf("%d", &numero);
 
     if (numero % 2 == 0) {
-        printf("O número %d é par.\n", numero);
+        printf("O numero %d e par.\n", numero);
     } else {
-        printf("O número %d é ímpar.\n", numero);
+        printf("O numero %d e impar.\n", numero);
     }
 
     return 0;
